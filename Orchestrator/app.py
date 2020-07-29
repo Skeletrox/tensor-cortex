84ad01b01114b112dcf62e1e7c93c27691773fc9
Skeletrox <android.svr@gmail.com>
2020-07-28 23:20:52 -0700

    introduce threads

--- Orchestrator/app.py
+++ Orchestrator/app.py
@@ -7,6 +7,7 @@ import random
 import asyncio
 import aiohttp
 import string
+from threading import Thread
 from os import environ
 from sys import exit
 
@@ -19,12 +20,39 @@ with open('./config.json') as config:
 DOCKER_URL = "http://{}".format(environ["DOCKER_HOST"])
 
 envs = None
+results = None
 
+# List of executing tasks. New tasks will only be added to this when empty
+tasks = []
+
+# List of sessions pertaining to above tasks. Similar constraints apply.
+sessions = []
+
+# thread pool
+thread_pool = []
+
+
+def do_awaiting_wrapper(awaitables):
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(do_awaiting(awaitables))
+    awaitables = []
+
+async def do_awaiting(awaitables):
+    global results
+    if not results:
+        return
+    results = await asyncio.gather(*awaitables, return_exceptions=True)
 
 # Random string generator to uniquely identify each docker instance
 def randomString(stringLength=8):
     letters = string.ascii_lowercase
     return "".join([random.choice(letters) for i in range(stringLength)])
+
+
+def join_all_threads():
+    global thread_pool
+    for t in thread_pool:
+        t.join()
 
 
 async def fetchJson(session, url, jsonData=None, method=None):
@@ -37,31 +65,35 @@ async def fetchJson(session, url, jsonData=None, method=None):
 
 
 async def rollCallAsync():
-    tasks = []
+    rc_tasks = []
+    rc_sessions = []
     ports = metadata["containers"]
     for i in range(len(ports)):
         session = aiohttp.ClientSession()
         url = "{}:{}/getname".format(DOCKER_URL, ports[i])
-        tasks.append(fetchJson(session, url))
-
-    res = await asyncio.gather(*tasks, return_exceptions=True)
+        rc_tasks.append(fetchJson(session, url))
+        rc_sessions.append(session)
+    res = await asyncio.gather(*rc_tasks, return_exceptions=True)
+    for session in rc_sessions:
+        await session.close()
     return res
 
 
-async def demuxRuns(data):
+def demuxRuns(data):
+    global tasks, sessions, thread_pool
+    if tasks:
+        return
     ports = metadata["containers"]
-    tasks = []
-    sessions = []
     for i in range(len(ports)):
         session = aiohttp.ClientSession()
         jsonData = {"source": data["sources"][i], "count": data["counts"][i]}
         url = "{}:{}/dry_run".format(DOCKER_URL, ports[i])
-        tasks.append(fetchJson(session, url, jsonData, "POST"))
+        tasks.append(fetchJson(session,url,jsonData,"POST"))
         sessions.append(session)
-    res = await asyncio.gather(*tasks, return_exceptions=True)
-    for session in sessions:
-        await session.close()
-    return res
+
+    task_thread = Thread(target=do_awaiting_wrapper,args=[tasks])
+    task_thread.start()
+    thread_pool.append(task_thread)
 
 
 @app.route('/')
@@ -71,20 +103,32 @@ def hello():
 
 @app.route('/steps', methods=["POST"])
 def demux():
-    returnable = []
+    global tasks
+    if len(tasks) > 0:
+        return jsonify({
+            "error": "tasks still running"
+        }), 429
+
     with open("/var/log/steps.log", "a") as s:
         data = request.json
-        returnable = []
-        loop = asyncio.new_event_loop()
+        # loop = asyncio.new_event_loop()
         # asyncio.set_event_loop(loop)
-        returned = loop.run_until_complete(demuxRuns(data))
-        for r in returned:
-            try:
-                returnable.append(json.loads(r))
-            except Exception as e:
-                s.write(str(e) + "\n")
+        # returned = loop.run_until_complete(demuxRuns(data))
+        demuxRuns(data)
 
-    return jsonify({"responses": returnable})
+    return jsonify({
+        "success": True,
+        "tasks_dispatched": len(tasks),
+        "sessions_dispatched": len(sessions)
+    }), 200
+
+
+@app.route('/num_tasks', methods=["GET"])
+def getTasks():
+    global tasks
+    return jsonify({
+        "num_tasks": len(tasks)
+    }), 200
 
 
 @app.route('/init')
@@ -102,7 +146,7 @@ def initWorkers():
                 "id": value
             })
         returnable[value] = r.json()
-    return jsonify({"responses": returnable})
+    return jsonify({"responses": returnable}), 200
 
 
 @app.route('/rollcall')
@@ -110,7 +154,7 @@ def rollCall():
     with open('/var/log/rollcall.log', 'a') as r:
         loop = asyncio.new_event_loop()
         returnable = loop.run_until_complete(rollCallAsync())
-    return jsonify({"responses": [json.loads(r) for r in returnable]})
+    return jsonify({"responses": [json.loads(r) for r in returnable]}), 200
 
 
 @app.route('/dry_runs', methods=["POST"])
@@ -121,7 +165,7 @@ def dry_runs():
         r = requests.get(url="{}:{}/dry_run".format(DOCKER_URL, ports[i]))
         returnable[i] = r.json()
 
-    return jsonify({"responses": returnable})
+    return jsonify({"responses": returnable}), 200
 
 
 if __name__ == "__main__":
